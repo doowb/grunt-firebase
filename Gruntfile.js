b222798eee1a997c127c5bb46193a093b88ed661
Brian Woodward <brian.woodward@gmail.com>
2013-08-01 01:40:08 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+/*
+ * grunt-firebase
+ * https://github.com/upstage/grunt-firebase
+ *
+ * Copyright (c) 2013 Upstage
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  var authConfig = grunt.file.readJSON('./config/auth.json');
+
+  // Project configuration.
+  grunt.initConfig({
+
+    authConfig: authConfig,
+
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp'],
+    },
+
+    // Configuration to be run (and then tested).
+    firebase: {
+      options: {
+        // reference to start with (full firebase url)
+        reference: 'https://brandscale.firebaseio.com/demo',
+        // token is the secret key used for connecting to firebase from the server
+        // this is redacted from the public repo... add a file called ./config/auth.json
+        // with your token in it... { "token": "my token here" }
+        token: '<%= authConfig.token %>'
+      },
+      load: {
+        options: {
+          data: {
+            one: {
+              foo: 'bar'
+            },
+            two: [
+              { a: 'A' },
+              { b: 'B' },
+              { c: 'C' }
+            ],
+            three: [ 'first', 'second', 'third' ]
+          }
+        }
+      }
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'firebase']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/firebase.js
@@ -0,0 +1,74 @@
+/*
+ * grunt-firebase
+ * https://github.com/upstage/grunt-firebase
+ *
+ * Copyright (c) 2013 Upstage
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  var _ = require('lodash');
+  var Firebase = require('firebase');
+
+  var validation = {
+    reference: 'Define a firebase URL to upload data to.',
+    token: 'Define a token used to authenticate against the firebase reference.'
+  };
+
+  var validateOptions = function(options, cb) {
+    var errs = [];
+
+    _.forOwn(validation, function(msg, k) {
+      if(!options[k]) {
+        errs.push({option: k, msg: msg});
+      }
+    });
+
+    if(errs.length === 0) {
+      cb(null, true);
+    } else {
+      cb(errs, false);
+    }
+  };
+
+  grunt.registerMultiTask('firebase', 'Update your firebase.', function() {
+    
+    var done = this.async();
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      data: {}
+    });
+
+    validateOptions(options, function(errs, valid) {
+      if(errs) {
+        errs.forEach(function(err) {
+          grunt.warn('options.' + err.option + ' undefined: ' + err.msg);
+        });
+        done(false);
+      }
+    });
+
+    // create a new firebase reference using the reference url
+    var ref = new Firebase(options.reference);
+
+    // authenticate to firebase with the token
+    ref.auth(options.token, function(err, result) {
+      if(err) {
+        grunt.warn('Firebase login failed: ', err);
+        done(false);
+      }
+
+      // update firebase with the data
+      ref.update(options.data);
+
+      done();
+
+    });
+
+  });
+
+};
